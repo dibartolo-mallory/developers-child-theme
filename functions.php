ff2c4be4d4ae149c38957ae8e88dc5a8a361e1a7
dibartolo-mallory <dib17003@byui.edu>
2018-12-08 11:01:15 -1000

    bug fixes

--- functions.php
+++ functions.php
@@ -5,13 +5,33 @@
  * Date: 11/6/18
  * Time: 8:29 PM
  */
-namespace mdbcreative;
 
-include_once( 'lib/init.php' );
+include_once('lib/init.php');
 
-include_once( 'lib/functions/autoload.php' );
-
+include_once('lib/functions/autoload.php');
 
 
 
 
+add_action( 'genesis_after_header', 'home_page_banner' );
+
+/** Banner Image on Home Page
+ *
+ * @author Mallory Di Bartolo
+ *
+ *
+ */
+
+function home_page_banner() {
+
+    if ( ! is_front_page() ) {
+        return;
+    } ?>
+
+
+    <div class="home-banner">
+        <img src="http://placehold.it/1600x222&amp;text=Banner" alt="Banner" />
+    </div>
+
+    <?php
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var gulp = require('gulp'),
+
+    // Sass/CSS processes
     bourbon = require('bourbon').includePaths,
     neat = require('bourbon-neat').includePaths,
     sass = require('gulp-sass');
 
-gulp.task('styles', function(){
-
+gulp.task('styles', function() {
     return gulp.src('assets/sass/style.scss')
         .pipe(sass({
-            includePaths: [].concat(bourbon, neat),
+            includePaths: [].concat( bourbon, neat ),
             errLogToConsole: true,
             outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
         }))
 
         .pipe(gulp.dest('./'));
-
 });
 
 gulp.task('watch', function () {
